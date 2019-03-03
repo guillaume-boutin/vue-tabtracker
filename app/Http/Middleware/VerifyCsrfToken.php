5da0a964865ac1a5ae3757ffbeb2a4bdc0a7bb5c
Guillaume Boutin <guillaumeboutin2101@gmail.com>
2019-03-02 20:02:23 -0500

    Edited seeder and migration for Users table.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var bool
      */
-    protected $addHttpCookie = true;
+    protected $addHttpCookie = env('APP_ENV') !== 'local';
 
     /**
      * The URIs that should be excluded from CSRF verification.
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'email' => 'john@doe.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
